10f996cd54db44226bca1625f644dbf5aa50ffce
perrinletembet <perrinletembet.luquet@gmail.com>
2025-08-08 15:23:48 +0100

    Initial Python introduction project files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {product}")
