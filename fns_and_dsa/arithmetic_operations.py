7d10536bbb53d695b4dae4f064b0c3fbae572bdf
perrinletembet <perrinletembet.luquet@gmail.com>
2025-08-30 14:12:40 +0100

    Error on division fixed

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -17,8 +17,8 @@ def perform_operation(num1, num2, operation):
   elif operation == 'multiply':
     return num1 * num2
   elif operation == 'divide':
-    if num2 != 0:
-      return num1 / num2
-    return "Error: Division by zero"
+    if num2 == 0:
+      return "Error: Division by zero"
+    return num1 / num2
   else:
     return "Error: Invalid operation"
