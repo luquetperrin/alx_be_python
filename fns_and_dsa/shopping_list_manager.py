ac473cc1a375fa38723079debf74a010feb4308c
perrinletembet <perrinletembet.luquet@gmail.com>
2025-08-30 15:14:12 +0100

    test

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
 
         elif choice == '2':
-            item = input("Enter item: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
@@ -35,6 +35,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
